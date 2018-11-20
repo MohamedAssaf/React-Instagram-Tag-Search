80df7c3f9e04ebdaeb634f58707d072e5c8333dd
Mohamed Assaf <m.assemassaf@gmail.com>
2018-11-20 03:28:47 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.d26e4f4a417d0be5f8b3342a656f2023.js"
+  "/React-Instagram-Tag-Search/precache-manifest.d7b2f44c9e6113c0c11e9db7d411482d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/React-Instagram-Tag-Search/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
